4b458164c533cc04f0e25ae660ba57808cceb6b5
216k155 <216k155@luxcore.io>
2018-06-06 06:05:41 +1000

    Assign segwit start time for testing

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -141,11 +141,11 @@ public:
         consensus.nLastPOWBlock = 6000000;
         // Deployment of SegWit (BIP141 and BIP143)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1528226239;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1528234050; // 1hr from now
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1557187200; // TODO: ?? - just some random date - 05.07.2019
         //TODO: set CSV parameters for mainnet
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1528226239; //TODO: ?
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1528234050; //TODO: ?
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1557187200; // TODO: ?? - just some random date - 05.07.2019
 
         //SMART_CONTRACTS_HARDFORK deployment does not require start time and timeout, because it uses block number
